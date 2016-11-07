aab656255df5c56b38b350355f3a3b5f6050a8c4
Dennis <dlin2727@gmail.com>
2016-11-07 10:49:28 -0500

    Create symptoms.js

---
+++ symptoms.js
@@ -0,0 +1,28 @@
+var symptoms = {
+
+
+description:[ "SYMPTOMS: Please indicate any symptoms you experienced regularly at any time prior to your injury (pre-injury) and symptoms you experienced after your injury (post-injury). Leave any necessary comments."
+],
+
+
+symptomObj:[
+{ "symptomName":"Headaches", 
+"pre":false , 
+"post":false , 
+"info":"" },
+{ "symptomName":"Pressure in head",
+ "pre":false,
+ "post":false,
+ "info":"" }, 
+{ "symptomName":"Nausea/vomiting",
+ "pre":false,
+ "post":false,
+ "info":"" }, 
+{ 
+"symptomName":"SYMPTOMNAME",
+ "pre":false,
+ "post":false, 
+"info":"" } 
+]};
+
+

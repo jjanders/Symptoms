af7171235d35195ba441212f0e37c6836de10d32
Dennis <dlin2727@gmail.com>
2016-11-11 23:11:35 -0500

    Update symptoms.js

--- symptoms.js
+++ symptoms.js
@@ -1,22 +1,25 @@
-var JSONsrc = JSON.parse('{"symptoms": {"description": ["SYMPTOMS: Please indicate any symptoms you experienced regularly at any time prior to your injury (pre-injury) and symptoms you experienced after your injury (post-injury). Leave any necessary comments."], "symptomObj": [{"symptomName": "Headaches", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "If so, what medications are you taking and how often?"} }, {"symptomName": "Pressure in head", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Nausea/vomiting", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sensitivity to light", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sensitivity to noise", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Dizziness/vertigo", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Slowed down", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "In a fog", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Not right", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Difficulty concentration", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Memory difficulties", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Fatigued or low energy", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Confused", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Irritable", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sad", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Nervous", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Anxious", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Drowsy", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sleeping more than usual", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "If so, have you been napping, and how much?"} }, {"symptomName": "Sleeping less than usual", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Balance problems", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Neck pain", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }] } }'); 
+var JSONsrc = JSON.parse('{"symptoms": {"description": ["SYMPTOMS: Please indicate any symptoms you experienced regularly at any time prior to your injury (pre-injury) and symptoms you experienced after your injury (post-injury). Leave any necessary comments."],"headers":["Symptoms","Pre-Injury","Post-Injury"], "symptomObj": [{"symptomName": "Headaches", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "If so, what medications are you taking and how often?"} }, {"symptomName": "Pressure in head", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Nausea/vomiting", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sensitivity to light", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sensitivity to noise", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Dizziness/vertigo", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Slowed down", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "In a fog", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Not right", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Difficulty concentration", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Memory difficulties", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Fatigued or low energy", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Confused", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Irritable", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sad", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Nervous", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Anxious", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Drowsy", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Sleeping more than usual", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "If so, have you been napping, and how much?"} }, {"symptomName": "Sleeping less than usual", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Balance problems", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }, {"symptomName": "Neck pain", "pre": {"type": "checkbox"}, "post": {"type": "checkbox"}, "info": {"type": "string", "text": "Additional Comments"} }] } }'); 
+
+
 var desc = document.getElementById('desc');
 desc.innerHTML = JSONsrc["symptoms"]["description"];
 
 
 var tablerow = document.createElement('tr');
 var t1 = document.createElement('th');
-t1.innerHTML = "Symptom Name";
+t1.innerHTML = JSONsrc["symptoms"]["headers"][0];
 var t2 = document.createElement('th');
-t2.innerHTML = "Pre-Injury";
+t2.innerHTML = JSONsrc["symptoms"]["headers"][1];
 var t3 = document.createElement('th');
-t3.innerHTML = "Post-Injury";
+t3.innerHTML = JSONsrc["symptoms"]["headers"][2];
 
 
 tablerow.appendChild(t1);
 tablerow.appendChild(t2);
 tablerow.appendChild(t3);
 container.appendChild(tablerow);
-//if (checkbox.value == 0){;
+
+
 for(i=0; i < JSONsrc["symptoms"]["symptomObj"].length; i++){
 	var tr = document.createElement('tr');
 	var td1 = document.createElement('td');
@@ -27,17 +30,18 @@ for(i=0; i < JSONsrc["symptoms"]["symptomObj"].length; i++){
 
 	var checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
-
+	checkbox.id = "pre"+i;
 
 	var checkbox2 = document.createElement('input');
 	checkbox2.type = 'checkbox';
-
+	checkbox2.id = "post"+i;
 
 	var inputString = document.createElement('input');
 	inputString.type = 'string';
 
 
 	td1.innerHTML = JSONsrc["symptoms"]["symptomObj"][i].symptomName;
+
 	if(JSONsrc["symptoms"]["symptomObj"][i].pre.type = checkbox){
 	td2.appendChild(checkbox);
 	}
@@ -56,5 +60,31 @@ for(i=0; i < JSONsrc["symptoms"]["symptomObj"].length; i++){
 	}
 	container.appendChild(tr);
 
-
 }
+
+var storeLocal = function(){
+ var inputAll = document.querySelectorAll("input");
+ 	for(k = 0; k < inputAll.length; k++){
+ 		if (inputAll[k].type=="checkbox"){
+ 		   localStorage.setItem(inputAll[k].id,inputAll[k].checked);
+ 		}
+ 	}
+}
+
+var restoreLocal = function(){
+	var inputAll = document.querySelectorAll("input");
+	 for(k = 0; k < inputAll.length; k++){
+ 		if (inputAll[k].type=="checkbox"){
+			document.getElementById(inputAll[k].id).checked = localStorage.getItem(inputAll[k].id);
+ 		}
+ 	}
+}
+
+var clearLocal = function(){
+	localStorage.clear();
+}
+
+document.getElementById("save").addEventListener("click",storeLocal);
+document.getElementById("check").addEventListener("click",restoreLocal);
+document.getElementById("clear").addEventListener("click",clearLocal);
+
